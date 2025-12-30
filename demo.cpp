ca5a1091ac615fea7ce3c0063a519412651c5e07
xyc0833 <2031362123@qq.com>
2025-12-30 11:38:54 +0800

    新增了一些内容

--- demo.cpp
+++ demo.cpp
@@ -1,1 +1,19 @@
-// 
+// dsfasdfsf
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numbers = {5, 2, 9, 1, 5, 6};
+
+    std::sort(numbers.begin(), numbers.end());
+
+    std::cout << "Sorted numbers: ";
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
